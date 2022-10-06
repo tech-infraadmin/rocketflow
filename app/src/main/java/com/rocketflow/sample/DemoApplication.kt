2b1d450746b505a7790a35e5a2dd14beae7f9a42
Farhan <mfarhan@firstorion.com>
2022-10-07 01:31:20 +0400

    update latest SDK version 2.0.4

--- app/src/main/java/com/rocketflow/sample/DemoApplication.kt
+++ app/src/main/java/com/rocketflow/sample/DemoApplication.kt
@@ -2,13 +2,8 @@ package com.rocketflow.sample
 
 import android.app.Application
 import com.rocketflow.sdk.RocketFlyer
-import com.tracki.TrackiApplication
-import dagger.android.HasActivityInjector
-import dagger.android.HasBroadcastReceiverInjector
-import dagger.android.HasServiceInjector
 
-class DemoApplication : TrackiApplication(), HasActivityInjector,
-    HasServiceInjector, HasBroadcastReceiverInjector {
+class DemoApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         RocketFlyer.initializeRocketFlyer(this)
